6d1c81bc831f4ff5d80cf126b2be746f3e75ae17
threezinedine <threezinedine@gmail.com>
2026-06-23 07:28:01 +0700

    [setup] initial setup

---
+++ analyze/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from analyze!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ config.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ntt-3d-graphical-engine!")
+
+
+if __name__ == "__main__":
+    main()
