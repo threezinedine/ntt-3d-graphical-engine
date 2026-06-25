bb3e8615478e68acc4abefab61bbcd95b36e5efa
threezinedine <threezinedine@gmail.com>
2026-06-25 21:17:55 +0700

    [feature] insert after for list

--- engine/core/containers/list.h
+++ engine/core/containers/list.h
@@ -17,6 +17,80 @@ private:
 	};
 
 public:
+	struct Iterator
+	{
+	public:
+		Iterator(Node* pNode)
+			: m_pCurrent(pNode)
+		{
+		}
+
+		Iterator(const Iterator& other)
+			: m_pCurrent(other.m_pCurrent)
+		{
+		}
+
+		Iterator(Iterator&& other) noexcept
+			: m_pCurrent(other.m_pCurrent)
+		{
+			other.m_pCurrent = nullptr;
+		}
+
+		void operator=(const Iterator& other)
+		{
+			if (this != &other)
+			{
+				m_pCurrent = other.m_pCurrent;
+			}
+		}
+
+		void operator=(Iterator&& other) noexcept
+		{
+			if (this != &other)
+			{
+				m_pCurrent		 = other.m_pCurrent;
+				other.m_pCurrent = nullptr;
+			}
+		}
+
+		T& operator*() const
+		{
+			return m_pCurrent->data;
+		}
+
+		Iterator& operator++()
+		{
+			if (m_pCurrent != nullptr)
+			{
+				m_pCurrent = m_pCurrent->pNext;
+			}
+			return *this;
+		}
+
+		void operator++(int)
+		{
+			Iterator temp = *this;
+			++(*this);
+		}
+
+		bool operator==(const Iterator& other) const
+		{
+			return m_pCurrent == other.m_pCurrent;
+		}
+
+		bool operator!=(const Iterator& other) const
+		{
+			return !(*this == other);
+		}
+
+	public:
+		friend class List<T>;
+
+	private:
+		Node* m_pCurrent;
+	};
+
+public:
 	List(IAllocator* pAllocator = nullptr)
 		: m_pHead(nullptr)
 		, m_pTail(nullptr)
@@ -48,57 +122,25 @@ public:
 		}
 	}
 
-	Result Clear()
-	{
-		Node* pCurrent = m_pHead;
-
-		while (pCurrent != nullptr)
-		{
-			Node* pNext = pCurrent->pNext;
-			pCurrent->~Node(); // Call the destructor for the node
-			NTT_ASSERT_RESULT_SUCCESS(ALLOCATOR_SAFE(m_pAllocator)->Free(pCurrent, sizeof(Node)));
-			pCurrent = pNext;
-		}
-
-		m_pHead = nullptr;
-		m_pTail = nullptr;
-		m_Count = 0;
-
-		return RESULT_SUCCESS;
-	}
-
-	Result Append(T&& value)
-	{
-		Node* pNewNode = static_cast<Node*>(ALLOCATOR_SAFE(m_pAllocator)->Allocate(sizeof(Node)));
-
-		if (pNewNode == nullptr)
-		{
-			return RESULT_OUT_OF_MEMORY;
-		}
-
-		pNewNode->data	= static_cast<T&&>(value);
-		pNewNode->pPrev = m_pTail;
-		pNewNode->pNext = nullptr;
-
-		if (m_pTail != nullptr)
-		{
-			m_pTail->pNext = pNewNode;
-		}
-		else
-		{
-			m_pHead = pNewNode;
-		}
-
-		m_pTail = pNewNode;
-		m_Count++;
-
-		return RESULT_SUCCESS;
-	}
-
 	inline u32 GetCount() const
 	{
 		return m_Count;
 	}
+
+	inline Iterator Begin() const
+	{
+		return Iterator(m_pHead);
+	}
+
+	inline Iterator End() const
+	{
+		return Iterator(nullptr);
+	}
+
+	Result Clear();
+	Result Append(T&& value);
+	Result InsertAfter(Iterator position, T&& value);
+	T&	   operator[](u32 index);
 
 private:
 	Node*		m_pHead;
@@ -106,5 +148,110 @@ private:
 	u32			m_Count;
 	IAllocator* m_pAllocator;
 };
+
+template <typename T>
+Result List<T>::Append(T&& value)
+{
+	Node* pNewNode = static_cast<Node*>(ALLOCATOR_SAFE(m_pAllocator)->Allocate(sizeof(Node)));
+
+	if (pNewNode == nullptr)
+	{
+		return RESULT_OUT_OF_MEMORY;
+	}
+
+	pNewNode->data	= static_cast<T&&>(value);
+	pNewNode->pPrev = m_pTail;
+	pNewNode->pNext = nullptr;
+
+	if (m_pTail != nullptr)
+	{
+		m_pTail->pNext = pNewNode;
+	}
+	else
+	{
+		m_pHead = pNewNode;
+	}
+
+	m_pTail = pNewNode;
+	m_Count++;
+
+	return RESULT_SUCCESS;
+}
+
+template <typename T>
+Result List<T>::Clear()
+{
+	Node* pCurrent = m_pHead;
+
+	while (pCurrent != nullptr)
+	{
+		Node* pNext = pCurrent->pNext;
+		pCurrent->~Node(); // Call the destructor for the node
+		NTT_ASSERT_RESULT_SUCCESS(ALLOCATOR_SAFE(m_pAllocator)->Free(pCurrent, sizeof(Node)));
+		pCurrent = pNext;
+	}
+
+	m_pHead = nullptr;
+	m_pTail = nullptr;
+	m_Count = 0;
+
+	return RESULT_SUCCESS;
+}
+
+template <typename T>
+Result List<T>::InsertAfter(typename List<T>::Iterator position, T&& value)
+{
+	Node* pCurrent = position.m_pCurrent;
+
+	Node* pNewNode = (Node*)ALLOCATOR_SAFE(m_pAllocator)->Allocate(sizeof(Node));
+
+	if (pNewNode == nullptr)
+	{
+		return RESULT_OUT_OF_MEMORY;
+	}
+
+	pNewNode->data	= static_cast<T&&>(value);
+	pNewNode->pPrev = pCurrent;
+	pNewNode->pNext = (pCurrent != nullptr) ? pCurrent->pNext : nullptr;
+
+	if (pCurrent != nullptr)
+	{
+		if (pCurrent->pNext != nullptr)
+		{
+			pCurrent->pNext->pPrev = pNewNode;
+		}
+		pCurrent->pNext = pNewNode;
+	}
+	else
+	{
+		if (m_pHead == nullptr)
+		{
+			m_pHead = pNewNode;
+		}
+		else
+		{
+			m_pTail->pNext = pNewNode;
+		}
+		m_pTail = pNewNode;
+	}
+
+	m_Count++;
+
+	return RESULT_SUCCESS;
+}
+
+template <typename T>
+T& List<T>::operator[](u32 index)
+{
+	NTT_ASSERT_MSG(index < m_Count, "Index out of bounds");
+
+	Node* pCurrent = m_pHead;
+	for (u32 i = 0; i < index; ++i)
+	{
+		pCurrent = pCurrent->pNext;
+	}
+
+	return pCurrent->data;
+}
 
 } // namespace ntt
--- engine/main/unittest/tests/containers/test_list.cpp
+++ engine/main/unittest/tests/containers/test_list.cpp
@@ -73,3 +73,59 @@ TEST_CASE(ListTest, ClearList)
 	TEST_EQUAL(TestObject::s_CopyConstructorCount, 0);
 	TEST_EQUAL(TestObject::s_MoveConstructorCount, 3);
 }
+
+TEST_CASE(ListTest, Iterator)
+{
+	List<i32> list;
+
+	TEST_SUCCESS(list.Append(1));
+	TEST_SUCCESS(list.Append(2));
+	TEST_SUCCESS(list.Append(3));
+
+	i32 expectedValue = 1;
+	for (auto it = list.Begin(); it != list.End(); ++it)
+	{
+		TEST_EQUAL(*it, expectedValue);
+		expectedValue++;
+	}
+
+	TEST_EQUAL(expectedValue, 4); // Ensure we iterated through all elements
+}
+
+TEST_CASE(ListTest, InsertAfter)
+{
+	List<i32> list;
+
+	TEST_SUCCESS(list.Append(1));
+	TEST_SUCCESS(list.Append(3));
+
+	auto it = list.Begin();
+	++it; // Move to the second element (3)
+
+	TEST_SUCCESS(list.InsertAfter(it, 2)); // Insert 2 after 3
+
+	TEST_EQUAL(list.GetCount(), 3);
+	TEST_EQUAL(list[0], 1);
+	TEST_EQUAL(list[1], 3);
+	TEST_EQUAL(list[2], 2);
+
+	list.Clear();
+
+	TEST_SUCCESS(list.Append(1));
+	TEST_SUCCESS(list.Append(2));
+
+	it = list.End(); // Iterator pointing to the end (nullptr)
+
+	TEST_SUCCESS(list.InsertAfter(it, 3)); // Insert 3 at the end
+
+	TEST_EQUAL(list.GetCount(), 3);
+	TEST_EQUAL(list[0], 1);
+	TEST_EQUAL(list[1], 2);
+	TEST_EQUAL(list[2], 3);
+
+	list.Clear();
+
+	TEST_SUCCESS(list.InsertAfter(list.Begin(), 1)); // Insert 1 after the first element (which is nullptr)
+	TEST_EQUAL(list.GetCount(), 1);
+	TEST_EQUAL(list[0], 1);
+}
